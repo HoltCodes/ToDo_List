00a2a1037bfec14e5f27f14e07a8bf91e707531a
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-03-24 17:03:04 -0700

    commit

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Form from "./components/Form"
+import Display from "./components/Display"
 
 function App() {
+
+  const [todoList, setToDoList] = useState([];)
+
   return (
     <div className="App">
+
+      <Form todoList={todoList} setToDoList={setToDoList}/>
+
+      <Display todoList={todoList} setToDoList={setToDoList}/>
 
     </div>
   );
---
+++ todo_list/src/components/Form.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const Form = (props) => {
+
+  const [todoText, setTodoText] = useState("");
+  const [todoList, setTodoList] = props;
+
+}
+
+
+
+
+
+
+
+
+export default Form;
