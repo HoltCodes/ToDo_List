9ae0a2b6b161519405ba4015b8d9aca811db1470
Your Name  git config --global user.email you@example.comgit config --global user.email holt.codes@gmail.com <you@example.com>
2022-03-26 15:53:28 -0700

    commit

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
 
+import ToDo from './components/ToDo';
+
 
 function App() {
 
@@ -9,8 +11,39 @@ function App() {
 
   const handNeWTodoSubmit = (event) => {
     event.preventDefault();
-    todos.push(newTodo);
+
+    if(newTodo.length === 0){
+      return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      complete: false
+    }
+
+
+    setTodos([...todos, todoItem]);
+    setNewToDo("");
   };
+
+  const handleTodoDelete = (delIdx) => {
+    const filteredTodos = todos.filter((todo, i) => {
+      return i !== delIdx;
+    });
+
+    setTodos(filteredTodos);
+  };
+
+  const handleToggleComplete = (idx) => {
+    const updatedTodos = todos.map((todo, i) => {
+      if (idx === i) {
+        todo.complete = ! todo.complete;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
 
   return (
     <div className="App">
@@ -23,11 +56,28 @@ function App() {
           setNewToDo(event.target.value);
         }}
          type="text"
+         value={newTodo}
+         style={{ marginTop: "10px"}}
          /> 
-         <div>
+         <div style={{ marginTop: "10px"}}>
            <button>Add</button>
+           
          </div>
       </form>
+
+      <hr/>
+
+      {todos.map((todo, i) => {
+        return (
+        <ToDo 
+        key={i} 
+        todo={todo} 
+        handleToggleComplete={handleToggleComplete} 
+        i={i}
+        handleTodoDelete={handleTodoDelete}
+        />
+        );
+      })}
     </div>
   );
 }
---
+++ todo_list/src/components/ToDo.js
@@ -0,0 +1,27 @@
+const ToDo = (props) => {
+  const todoClasses = [];
+
+  if (props.todo.complete) {
+    todoClasses.push("line-through");
+  }
+
+  return (
+  <div>
+    <input 
+      onChange={(event) => {
+      props.handleToggleComplete(props.i);
+    }} 
+    checked={props.todo.complete} type="checkbox"/>
+    <span className={todoClasses.join(" ")}> {props.todo.text} </span>
+    <button 
+      onClick={(event) => {
+      props.handleTodoDelete(props.i);
+    }}>
+      Delete
+    </button>
+</div> 
+  );
+};
+
+
+export default ToDo;
